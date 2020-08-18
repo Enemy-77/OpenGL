12bf620f8389da6413a510e2f413629f8e4ff5df
giantdig <1914118343@qq.com>
2020-08-18 21:16:20 +0800

    abstract vertex array

--- OpenGL/OpenGL/src/Application.cpp
+++ OpenGL/OpenGL/src/Application.cpp
@@ -9,6 +9,7 @@
 #include "Renderer.h"
 #include "VertexBuffer.h"
 #include "IndexBuffer.h"
+#include "VertexArray.h"
 
 struct ShaderProgramSource
 {
@@ -133,11 +134,11 @@ int main(void)
             2, 3, 0
         };
 
-        unsigned int vao;
-        GLCall(glGenVertexArrays(1, &vao));
-        GLCall(glBindVertexArray(vao));
-
+        VertexArray va;
         VertexBuffer vb(position, 4 * 2 * sizeof(float));
+        VertexBufferLayout layout;
+        layout.Push<float>(2);
+        va.AddBuffer(vb, layout);
 
         glEnableVertexAttribArray(0);
         ///第二个参数是组成一个类型的参数的顶点属性，本例中是2
@@ -157,6 +158,7 @@ int main(void)
         ASSERT(location != -1);
         glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f);
 
+        va.Unbind();
         GLCall(glUseProgram(0));
         GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
         GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
@@ -171,11 +173,8 @@ int main(void)
 
             GLCall(glUseProgram(shader));
             glUniform4f(location, r, 0.3f, 0.8f, 1.0f);
-            //         
-            //         vb.Bind();
-            //         GLCall(glEnableVertexAttribArray(0));
-            //         GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
 
+            va.Bind();
             ib.Bind();
 
             GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
@@ -193,10 +192,8 @@ int main(void)
             /* Poll for and process events */
             glfwPollEvents();
         }
-
         glDeleteProgram(shader);
     }
-
     glfwTerminate();
     return 0;
 }
---
+++ OpenGL/OpenGL/src/VertexArray.cpp
@@ -0,0 +1,41 @@
+#include "VertexArray.h"
+#include "Renderer.h"
+
+VertexArray::VertexArray()
+{
+    GLCall(glGenVertexArrays(1, &m_RendererID));
+}
+
+VertexArray::~VertexArray()
+{
+    GLCall(glDeleteVertexArrays(1, &m_RendererID));
+}
+
+void VertexArray::AddBuffer(const VertexBuffer& vb, const VertexBufferLayout& layout)
+{
+    Bind();
+    vb.Bind();
+    const auto& elements = layout.GetElements();
+    for (unsigned int i = 0; i < elements.size(); ++i)
+    {
+        const auto& element = elements[i];
+        unsigned int offset = 0;
+        GLCall(glEnableVertexAttribArray(i));
+        ///第二个参数是组成一个类型的参数的顶点属性，本例中是2
+        ///第五个参数是 stride，是指每一类 vertex 需要多大的空间，比如 position 两个float，texture需要两个float，normal需要两个float
+        ///第六个参数是 pointer，是指每个类型的顶点的第一个组成元素在数组中位置。是一个数字，要强化成指针。
+        ///告诉GPU数据的layout
+        GLCall(glVertexAttribPointer(i, element.count, element.type, element.normlized, layout.GetStride(), (const void*)offset));
+        offset += element.count * VertexBufferElement::GetSizeOfType(element.type);
+    }
+}
+
+void VertexArray::Bind() const
+{
+    GLCall(glBindVertexArray(m_RendererID));
+}
+
+void VertexArray::Unbind() const
+{
+    GLCall(glBindVertexArray(0));
+}
